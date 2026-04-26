e01e4f92aeca6b13de953323020f7847fc3a5fcc
Barbol <barbolmdp@gmail.com>
2026-04-26 19:33:47 +0100

    Graphic Visualization:
    -Added method to paint the background
    -draw_cell() now decodes the hex value for the wall to paint and paint
    them accordingly, using binary operations
    -cells with value 'F' are treated specially to highlight the 42 pattern
    in the center
    -colours added as class attributes for easy change

--- srcs/window.py
+++ srcs/window.py
@@ -4,30 +4,52 @@ from typing import Any, List
 
 class Window():
     """Testing class for graphical library"""
+    
     def __init__(self):
         self._m = Mlx()
         self._ptr = self._m.mlx_init()
         self._win = None
         self.fg_color: int = 0xeeeeeeff
-        self.bg_color: int = 0x222200ff
-        self.hl_color: int = 0x0000ffff
+        self.bg_color: int = 0x99ff55ff
+        self.hl_color: int = 0xff00ffff
         self.margin = 10
+    
+    def paint_bg(self, width: int, height: int):
+        # Paint bg:
+        for x in range(0, width * 20):
+            for y in range(0, height * 20):
+                self._m.mlx_pixel_put(
+                    self._ptr, self._win, self.margin + x, self.margin + y, self.bg_color)
 
     def draw_cell(self, x_offset: int, y_offset: int, value: str) -> None:
-        for x in range(0, 22):
-            for y in range(0, 22):
-                if x < 3  or x >= 19 or y < 3 or y >= 19:
-                    self._m.mlx_pixel_put(
-                            self._ptr, self._win, x + x_offset, y + y_offset, self.fg_color)
-                else:
-                    self._m.mlx_pixel_put(
-                            self._ptr, self._win, x + x_offset, y + y_offset, self.bg_color)
-                if x == 6 and y == 0:
-                    self._m.mlx_string_put(
-                            self._ptr, self._win, x + x_offset, y + y_offset,
-                            self.fg_color, value)
+        """Recieves a position to draw a cell and a hex value coding for
+        open and closed walls"""    
 
-
+        code = int('0x' + value, 16)
+        for x in range(0, 23):
+            for y in range(0, 23):
+                if y <= 3 and code & 0b0001:
+                    self._m.mlx_pixel_put(
+                        self._ptr, self._win, x + x_offset, y + y_offset, self.fg_color)
+                elif x >= 19 and code & 0b0010:
+                    self._m.mlx_pixel_put(
+                        self._ptr, self._win, x + x_offset, y + y_offset, self.fg_color)
+                elif y >= 19 and code & 0b0100:
+                    self._m.mlx_pixel_put(
+                        self._ptr, self._win, x + x_offset, y + y_offset, self.fg_color)
+                elif x <= 3 and code & 0b1000:
+                    self._m.mlx_pixel_put(
+                        self._ptr, self._win, x + x_offset, y + y_offset, self.fg_color)
+                elif value is "F":
+                    self._m.mlx_pixel_put(
+                            self._ptr, self._win, x + x_offset, y + y_offset, self.hl_color)
+#                else:
+#                    self._m.mlx_pixel_put(
+#                            self._ptr, self._win, x + x_offset, y + y_offset, self.bg_color)
+#                if x == 6 and y == 0:
+#                    self._m.mlx_string_put(
+#                            self._ptr, self._win, x + x_offset, y + y_offset,
+#                            self.fg_color, value)
 
     def render(self, maze: str) -> None:
         
@@ -45,7 +67,8 @@ class Window():
         self._win = self._m.mlx_new_window(self._ptr, 20 * width + 2 * self.margin,
                 20 * height + 2 * self.margin, "A_Maze_ing!")
         self._m.mlx_clear_window(self._ptr, self._win)
-        
+       
+        self.paint_bg(width, height)
         for x in range(0, width):
             for y in range(0, height):
                 self.draw_cell(20 * x + self.margin - 1, 20 * y + self.margin -1, rows[y][x])
