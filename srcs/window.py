db27fc015011bf9d8d06d34ff8350683e33b531b
Barbol <barbolmdp@gmail.com>
2026-04-27 23:15:33 +0100

    Graphic:
    -Color class moved to separate file
    -User interaction:
    	'f' to change color to 42 pattern

--- srcs/window.py
+++ srcs/window.py
@@ -1,47 +1,7 @@
 from mlx import Mlx
 from typing import Any, List, Tuple
 from .maze import Maze
-import random
-from enum import IntEnum
-
-
-class Color(IntEnum):
-    # Dark & Moody
-    CHARCOAL = 0xFF1a1a1a
-    DEEP_NAVY = 0xFF0a1428
-    DARK_SLATE = 0xFF2c3e50
-
-    # Rich Jewel Tones
-    EMERALD = 0xFF27ae60
-    SAPPHIRE = 0xFF2980b9
-    AMETHYST = 0xFF8e44ad
-    RUBY = 0xFFc0392b
-    
-    # Vibrant & Bold
-    NEON_CYAN = 0xFF00d4ff
-    NEON_MAGENTA = 0xFFff00ff
-    NEON_LIME = 0xFF00ff41
-    NEON_ORANGE = 0xFFff6600
-
-    # Elegant Neutrals
-    GRANITE = 0xFF555555
-    GUNMETAL = 0xFF2f4f4f
-    PEWTER = 0xFF708090
-
-    # Atmospheric
-    MIDNIGHT = 0xFF0f0f1e
-    FOREST = 0xFF1b4332
-    PLUM = 0xFF44355b
-    TEAL = 0xFF0d5f5f
-
-    # Basic
-    RED = 0xFF0000FF
-    GREEN = 0xFF00FF00
-    BLUE = 0xFFFF0000
-    BLACK = 0xFF000000
-
-    def get_random_color():
-        return random.choice(list(Color))
+from .color import Color
 
 
 class MazeWindow():
@@ -156,6 +116,11 @@ class MazeWindow():
             # 'c' to change walls color:
             if keynum == 99:
                 self.fg_color = Color.get_random_color()
+                self.draw_maze(maze.width, maze.height, maze.rows)
+
+            # 'f' to change walls color:
+            if keynum == 102:
+                self.hl_color = Color.get_random_color()
                 self.draw_maze(maze.width, maze.height, maze.rows)
 
         def close_window(stuff: Any):
