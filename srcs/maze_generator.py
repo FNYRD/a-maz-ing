0965e2ceac68189470d2c8f7ae468ab134f115a2
Jesus Rosales <jr.jobs.1509@gmail.com>
2026-04-29 15:37:25 +0100

    DFS listo
    Falta:
    
    - usar seed
    - usar perfect
    - verificar ancho de los pasillos

--- srcs/maze_generator.py
+++ srcs/maze_generator.py
@@ -1,5 +1,6 @@
 try:
     from typing import Tuple, List, Optional
+    import random
 except ImportError as e:
     print(f"An error happened importing the modules\n{e}")
 
@@ -8,8 +9,13 @@ class MazeTooSmallError(Exception):
     pass
 
 
+class MazeNotExistsError(Exception):
+    pass
+
+
 class MazeGenerator:
     """This constructor sets the arguments for the maze creation"""
+
     def __init__(self, width: int, height: int, entry: Tuple[int, int],
                  exit: Tuple[int, int], perfect: bool,
                  seed: Optional[int] = None) -> None:
@@ -19,24 +25,17 @@ class MazeGenerator:
         self.exit: Tuple[int, int] = exit
         self.perfect: bool = perfect
         self.seed: Optional[int] = seed
-        self.maze = [[0xf for _ in range(self.width)]
-                     for _ in range(self.height)]
+        self.maze: List[List[int]] = []
 
-    def show(self) -> List[List[int]]:
-        """This function shows the maze after setting
-         the entry and exit position"""
-        self.maze[self.entry[1]][self.entry[0]] = 5
-        self.maze[self.exit[1]][self.exit[0]] = 9
-        return self.maze
-
-    def pattern(self, offseth: int = 0, offsetw: int = 0,
-                direction: int = 0) -> None:
+    def __pattern(self, offseth: int = 0, offsetw: int = 0,
+                  direction: int = 0) -> None:
         """This function change the cell's value to 42 to
         print the 42 pattern"""
         if self.width <= 10 >= self.height:
             raise MazeTooSmallError("Maze's size is too small "
                                     "for displaying the 42 "
                                     "pattern")
+
         start_for: Tuple[int, int] = (((self.width // 2) - 1) + offsetw,
                                       (self.height // 2) + offseth)
         start_to: Tuple[int, int] = (((self.width // 2) + 1) + offsetw,
@@ -53,14 +52,14 @@ class MazeGenerator:
                 pattern_xy.append((start_to[1] + i, start_to[0] + 1))
                 pattern_xy.append((start_to[1] - i, start_to[0]))
         if entry_test in pattern_xy or exit_test in pattern_xy:
-            if start_for[1] - 2 >= 0 and direction != 2:
-                self.pattern(offseth - 1, 0, 1)
-            elif start_for[1] + 2 <= self.height and direction != 1:
-                self.pattern(offseth + 1, 0, 2)
-            elif start_for[0] - 2 >= 0 and direction != 4:
-                self.pattern(0, offsetw - 1, 3)
-            elif start_for[0] + 2 <= self.width and direction != 3:
-                self.pattern(0, offsetw + 1, 4)
+            if start_for[1] - 2 > 1 and direction != 2:
+                self.__pattern(offseth - 1, offsetw, 1)
+            elif start_for[1] + 2 < self.height and direction != 1:
+                self.__pattern(offseth + 1, offsetw, 2)
+            elif start_for[0] - 2 > 1 and direction != 4:
+                self.__pattern(offseth, offsetw - 1, 3)
+            elif start_for[0] + 2 < self.width and direction != 3:
+                self.__pattern(offseth, offsetw + 1, 4)
             else:
                 raise MazeTooSmallError("Maze's size is too small "
                                         "for displaying the 42 "
@@ -68,4 +67,69 @@ class MazeGenerator:
             return
         for coordinate in pattern_xy:
             self.maze[coordinate[0]][coordinate[1]] = 42
-        print(pattern_xy)
+
+    def generate(self) -> None:
+        self.maze = [[0xf for _ in range(self.width)]
+                     for _ in range(self.height)]
+        self.__pattern()
+
+    def __isvalid(self, position: Tuple[int, int]) -> List[Tuple[int, int]]:
+        x, y = position
+        options: List[Tuple[int, int]] = []
+        if 0 < y - 1 and 0 < x <= self.width - 2 and self.maze[y - 1][x] == 0xf:
+            options.append((x, y - 1))
+        if self.height - 1 > y + 1 and 0 < x <= self.width - 2 and self.maze[y + 1][x] == 0xf:
+            options.append((x, y + 1))
+        if 0 < x - 1 and 0 < y <= self.height - 2 and self.maze[y][x - 1] == 0xf:
+            options.append((x - 1, y))
+        if self.width - 1 > x + 1 and 0 < y <= self.height - 2 and self.maze[y][x + 1] == 0xf:
+            options.append((x + 1, y))
+        return options
+
+    def __opening_walls(self, current: Tuple[int, int],
+                        next: Tuple[int, int]) -> None:
+        cx, cy = current
+        nx, ny = next
+        if cy != ny:
+            if cy < ny:
+                self.maze[cy][cx] &= ~0x4
+                self.maze[ny][nx] &= ~0x1
+            elif cy > ny:
+                self.maze[ny][nx] &= ~0x4
+                self.maze[cy][cx] &= ~0x1
+            return
+        if cx < nx:
+            self.maze[cy][cx] &= ~0x2
+            self.maze[ny][nx] &= ~0x8
+        elif cx > nx:
+            self.maze[ny][nx] &= ~0x2
+            self.maze[cy][cx] &= ~0x8
+
+    def dfs(self) -> None:
+        """This algorithm will open radomly the walls till
+        find a path from entry to exit"""
+        if len(self.maze) == 0:
+            raise MazeNotExistsError(
+                "You cannot creat a maze with dimensions 0x0")
+        stack: List[Tuple[int, int]] = []
+        next: Tuple[int, int] = (0, 0)
+        current: Tuple[int, int] = self.entry
+        options: List[Tuple[int, int]]
+        while True:
+            options = self.__isvalid(current)
+            if len(options) > 0:
+                next = random.choice(options)
+                self.__opening_walls(current, next)
+                stack.append(current)
+                current = next
+                if next == self.exit:
+                    # BORRAR, ES SOLO PARA VER SI LLEGA A LA SALIDA
+                    self.maze[self.exit[1]][self.exit[0]] = 100
+                    break
+            else:
+                if len(stack) == 0:
+                    break
+                current = stack.pop()
+
+# FALTA:
+# SEED, PERFECT Y EL ANCHO DE LOS PASILLOS
--- srcs/test_generator.py
+++ srcs/test_generator.py
@@ -2,17 +2,21 @@
 try:
     from maze_generator import MazeGenerator, MazeTooSmallError
     from typing import List
+    import random
 except ImportError as e:
     print(f"An error happened importing the modules\n{e}")
 
-maze = MazeGenerator(10, 10, (0, 7), (6, 5), False)
+maze = MazeGenerator(11, 11, (1, 7), (6, 5), False)
 
 try:
-    maze.pattern()
-    maze: List[List[int]] = maze.show()
-    lines: int = len(maze)
-    for i in range(lines):
-        print(maze[i])
+    maze.generate()
+    print("WITHOUT DFS")
+    for row in maze.maze:
+        print(" ".join(f"{cell:2}" for cell in row))
+    print("\nWITH DFS")
+    maze.dfs()
+    for row in maze.maze:
+        print(" ".join(f"{cell:2}" for cell in row))
 except MazeTooSmallError as e:
     print(f"Error {e}")
 
@@ -21,25 +25,22 @@ except MazeTooSmallError as e:
 
 
 
-
-
-
-    # def pattern(self) -> None:
-    #     """This function change the cell's value to 42 to print the 42 pattern"""
-    #     if self.width <= 5 >= self.height:
-            # raise MazeTooSmallError("Maze's size is too small "
-            #                         "for displaying the 42 pattern")
-    #     start_for: Tuple[int, int] = (((self.width // 2) - 1), (self.height // 2))
-    #     start_to: Tuple[int, int] = (((self.width // 2) + 1), (self.height // 2))
-    #     pattern: List[Tuple[int, int]] = []
-    #     for i in range(3):
-    #         self.maze[start_for[1] + i][start_for[0]] = 42
-    #         self.maze[start_to[1] + i][start_to[0]] = 42
-    #         self.maze[start_to[1] - i][start_to[0] + 1] = 42
-    #         if i == 2:
-    #             self.maze[start_to[1] + i][start_to[0] + 1] = 42
-    #             self.maze[start_to[1] - i][start_to[0]] = 42
-    #     for i in range(2):
-    #         if i == 0:
-    #             self.maze[start_for[1] - i - 1][start_for[0]] = 42
-    #         self.maze[start_for[1] - i - 1][start_for[0] - 1] = 42
+# def pattern(self) -> None:
+#     """This function change the cell's value to 42 to print the 42 pattern"""
+#     if self.width <= 5 >= self.height:
+# raise MazeTooSmallError("Maze's size is too small "
+#                         "for displaying the 42 pattern")
+#     start_for: Tuple[int, int] = (((self.width // 2) - 1), (self.height // 2))
+#     start_to: Tuple[int, int] = (((self.width // 2) + 1), (self.height // 2))
+#     pattern: List[Tuple[int, int]] = []
+#     for i in range(3):
+#         self.maze[start_for[1] + i][start_for[0]] = 42
+#         self.maze[start_to[1] + i][start_to[0]] = 42
+#         self.maze[start_to[1] - i][start_to[0] + 1] = 42
+#         if i == 2:
+#             self.maze[start_to[1] + i][start_to[0] + 1] = 42
+#             self.maze[start_to[1] - i][start_to[0]] = 42
+#     for i in range(2):
+#         if i == 0:
+#             self.maze[start_for[1] - i - 1][start_for[0]] = 42
+#         self.maze[start_for[1] - i - 1][start_for[0] - 1] = 42
