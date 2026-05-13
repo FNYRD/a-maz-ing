fe77b1ea5d2b621282834092c10d54f639a4f34e
Sergio Gabriel Lenti <slenti@c2r3s11.42porto.com>
2026-05-13 15:19:45 +0100

    requirements update

--- venv/lib/python3.10/site-packages/mazegen/__init__.py
+++
@@ -1,4 +0,0 @@
-from mazegen.maze_generator import MazeGenerator, MazeNotExistsError
-from mazegen.maze_generator import MazeTooSmallError
-
-__all__ = ["MazeGenerator", "MazeTooSmallError", "MazeNotExistsError"]
